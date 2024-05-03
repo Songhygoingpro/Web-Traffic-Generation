ac1d5137fbf5e7fa014491719f6edef749b1a59f
Songhygoingpro <123635791+Songhygoingpro@users.noreply.github.com>
2024-05-03 14:15:14 +0700

    again

--- script.js
+++ script.js
@@ -7,15 +7,3 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for(let i=0; i<marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
-
-document.addEventListener("scroll", function() {
-  var pageTop = window.pageYOffset;
-  var pageBottom = pageTop + window.innerHeight;
-  var tags = document.querySelectorAll(".reveal");
-
-  tags.forEach(function(tag) {
-    if (tag.offsetTop < pageBottom) {
-      tag.classList.add("active");
-    } 
-  });
-});
